4e5aa0adf1e87f89f05da1a56d141be34e996550
Meris Williams <merisw@gmail.com>
2013-05-20 13:11:40 -0700

    Created Q&A

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CollaborativeQA::Application.config.secret_token = '4a6f8f36a4c3037f60b1e9b6f7fcfc1866a8e62a53cc299decad65c3b6da8abd67d12c137afbd7f5d699d5c2d644aead8252f97b0476e6ee69ee8130a8bc8b97'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CollaborativeQA::Application.config.session_store :cookie_store, key: '_collaborative_q_a_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CollaborativeQA::Application.config.session_store :active_record_store
